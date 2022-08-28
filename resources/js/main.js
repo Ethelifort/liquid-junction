4cee354b3d765bc0ddb301d868e2862f8eefb5e4
Ernst Thelifort <ethelifort@gmail.com>
2022-08-27 22:37:32 -0400

    Ingredients section added

--- resources/js/main.js
+++ resources/js/main.js
@@ -5,12 +5,12 @@ fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
 .then(data => {
 
 
-console.log(data);
 //Event Listener that will show the picture of the random drink the user has ended up with.
 //Event Listener that will fetch the next random drink on click.
 document.getElementById('next-button').addEventListener('click', nextDrink);
 
-console.log(data);
+
+
 
 function nextDrink(){
     if (data.drinks[0].strDrinkThumb){
@@ -23,7 +23,7 @@ function nextDrink(){
     newImage.src = data.drinks[0].strDrinkThumb;
       //Creating list
 
-      const li =  document.createElement('li');
+      let li =  document.createElement('li');
       //Placing Text into list
 
       li.textContent = data.drinks[0].strInstructions;
@@ -41,6 +41,26 @@ function nextDrink(){
    
       //append the li to the ol
       document.querySelector('ol').appendChild(li);
+//------------------------------------------------------------------------------------
+
+      li =  document.createElement('li');
+      //Placing Text into list
+
+      li.textContent = data.drinks[0].strIngredient1;
+        console.log(data);
+        console.log(li);
+      
+         //Creating the drinkname variable
+         drinkName.textContent = data.drinks[0].strDrink;
+         document.getElementById('ingredients').appendChild(drinkName);
+
+         //Inserts text into the h3
+         const ingredients = document.getElementById('ingredients');
+         ingredients.textContent = "Ingredients";
+
+   
+        //append the li to the ul
+        document.querySelector('ul').appendChild(li);
 
     return newImage.sr;
 
